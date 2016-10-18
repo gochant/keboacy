828231d2b4b915dc02cd212834359217602ad5e4
gochant <guochant@gmail.com>
2016-10-18 18:20:23 +0800

    add themes and widgets

---
+++ widgets/checkboxlist/demo/index.js
@@ -0,0 +1,15 @@
+var vm = {
+    val: '01',
+    list: [{
+        text: '选项1',
+        value: '01'
+    }, {
+        text: '选项2',
+        value: '02'
+    }, {
+        text: '选项3',
+        value: '03'
+    }]
+};
+
+kendo.bind($('body'), vm);
---
+++ widgets/dataTable/demo/index.js
@@ -0,0 +1,49 @@
+var vm = {
+    list: new kendo.data.DataSource({
+        data: [{
+            id: 1,
+            Name: '张三',
+            Gender: '男',
+            Age: 25
+        }, {
+            id: 2,
+            Name: '李四',
+            Gender: '女',
+            Age: 28
+        }, {
+            id: 3,
+            Name: 'ssx',
+            Gender: '女',
+            Age: 44
+        }, {
+            id: 4,
+            Name: '是的',
+            Gender: '男',
+            Age: 58
+        }, {
+            id: 5,
+            Name: 'mony',
+            Gender: '女',
+            Age: 24
+        }, {
+            id: 6,
+            Name: 'giss',
+            Gender: '男',
+            Age: 46
+        }, {
+            id: 7,
+            Name: 'goc',
+            Gender: '女',
+            Age: 85
+        }, {
+            id: 8,
+            Name: 'openx',
+            Gender: '男',
+            Age: 13
+        }]
+    })
+}
+
+kendo.bind($('body'), vm);
+
+vm.list.read();
